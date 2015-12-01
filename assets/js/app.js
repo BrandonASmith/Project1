4335f2e73f7a5625f45b65eceb4eeeb34233393c
Brandon Smith <basmith808@gmail.com>
2015-12-01 11:33:08 -0800

    working mvp

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,37 +1,25 @@
 var cell = "brandon",
 	obj1 = "",
 	obj2 = "",
-	board = {
-		cell1: "kick",
-		cell2: "snare",
-		cell3: "hat",
-		cell4: "piano",
-		cell5: "harp",
-		cell6: "synth",
-		cell7: "violin",
-		cell8: "horns",
-		cell9: "bass",
-		cell10: "shaker",
-		cell11: "kick",
-		cell12: "snare",
-		cell13: "hat",
-		cell14: "piano",
-		cell15: "harp",
-		cell16: "synth",
-		cell17: "violin",
-		cell18: "horns",
-		cell19: "bass",
-		cell20: "shaker"
+	turncount = 0;
+
+function play(param) {
+	if (turncount == 0) {
+		function click1(param) {
+			console.log(param.target.id)
+			obj1 = param.target.id;
+			turncount = 1;
+		}
+		click1(param);
+	} else if (turncount == 1 ) {
+		function click2(param) {
+			console.log(param.target.id)
+			obj2 = param.target.id;
+			turncount = 0;
+		}
+		click2(param);
+		checkMatch();
 	}
-
-function click1(param) {
-	console.log(param)
-	obj1 = param;
-}
-
-function click2(param) {
-	console.log(param)
-	obj2 = param;
 }
 
 function checkMatch() {
@@ -44,4 +32,4 @@ function checkMatch() {
 	obj2 = "";
 }
 
-
+$('.cell').on('click', play)
